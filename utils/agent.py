8bcab6f8c4ce8063ccb4aa7fe57250d9ecd48a04
Tuan-Duy H. Nguyen <v.duynht1@vinai.io>
2022-11-08 05:55:40 +0000

    fix agent

--- utils/agent.py
+++ utils/agent.py
@@ -3,6 +3,7 @@ import torch
 import utils
 from .other import device
 from model import ACModel
+from visual_transformer import CausalVisionTransformer
 
 
 class Agent:
@@ -15,7 +16,19 @@ class Agent:
     def __init__(self, obs_space, action_space, model_dir,
                  argmax=False, num_envs=1, use_memory=False, use_text=False):
         obs_space, self.preprocess_obss = utils.get_obss_preprocessor(obs_space)
-        self.acmodel = ACModel(obs_space, action_space, use_memory=use_memory, use_text=use_text)
+        # self.acmodel = ACModel(obs_space, action_space, use_memory=use_memory, use_text=use_text)
+        self.acmodel = CausalVisionTransformer(
+            in_channels=obs_space["image"][2],
+            out_channels=32,
+            kernel_size=3,
+            obs_space=obs_space,
+            action_space=action_space,
+            d_model=10,
+            nhead=1,
+            d_hid=10,
+            nlayers=2,
+            max_len=8
+        )
         self.argmax = argmax
         self.num_envs = num_envs
 
